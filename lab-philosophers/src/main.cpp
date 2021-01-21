d634e90e24c166fc6b9c921d1169aa597dc5b041
Jamabaiz <bartula.jan@gmail.com>
2021-01-21 22:59:20 +0100

    Philosophers

---
+++ lab-philosophers/src/main.cpp
@@ -0,0 +1,191 @@
+//
+// Created by bartu on 1/26/2021.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <stdbool.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <time.h>
+#include <thread>
+#include <mutex>
+#include <map>
+#include <atomic>
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <condition_variable>
+#include <iomanip>
+
+using namespace std;
+
+#define NO_PHILOSOPHERS 5
+bool DEBUG = 0;
+
+sem_t WAITER;
+sem_t CHOPSTICKS[NO_PHILOSOPHERS];
+
+
+mutex forks[NO_PHILOSOPHERS];
+
+atomic<int> tableCount;
+mutex tableLock;
+
+mutex printLock;
+condition_variable cv;
+
+bool chairAvaible() { return tableCount > 0; }
+
+int leftFork(int i) {
+    return i;
+}
+
+int rightFork(int i) {
+    return (i + 1) % NO_PHILOSOPHERS;
+}
+
+int randomSleep() {
+    return usleep(rand() % 1000);
+}
+
+class Philosopher {
+private:
+    int id;
+    string name;
+
+    mutex &leftFork(int i) {
+        return forks[i];
+    }
+
+    mutex &rightFork(int i) {
+        return forks[(i + 1) % NO_PHILOSOPHERS];
+    }
+
+public:
+
+    Philosopher();
+
+    Philosopher(int id) : id(id) { this->name = "Philosopher"; }
+
+    Philosopher(int id, string name) : id(id), name(name) {}
+
+    void randomSleep() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 500));
+    }
+
+    void secondsSleep(int seconds) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
+    }
+
+
+    void operator()() {
+
+        for (int i = 0; i < 2; i++) {
+
+            {
+                const lock_guard<mutex> lock(printLock);
+                cout << setw(20) << name << " id: " << id << ", is thinking 💤" << endl;
+            }
+
+            {
+                unique_lock<mutex> tempLock(tableLock);
+                auto sec = chrono::seconds(2);
+                if (!cv.wait_for(tempLock, 2 * sec, chairAvaible)) {
+                    cout << setw(20) << name << " id: " << id << ", starved to death" << endl;
+                    break;
+                }
+                tableCount--;
+            }
+
+
+            if (DEBUG) printf("Philosopher id: %i, joined the table\n", id);
+
+            randomSleep();
+            if (DEBUG) printf("Philosopher id: %i, acquiring LEFT fork\n", id);
+            leftFork(id).lock();
+            if (DEBUG) printf("Philosopher id: %i, acquired LEFT fork\n", id);
+
+            randomSleep();
+            if (DEBUG) printf("Philosopher id: %i, acquiring RIGHT fork\n", id);
+            rightFork(id).lock();
+            if (DEBUG) printf("Philosopher id: %i, acquired RIGHT fork\n", id);
+
+
+            if (DEBUG) printf("Philosopher id: %i, acquired forks\n", id);
+
+
+            {
+                const lock_guard<mutex> lock(printLock);
+                cout << setw(20) << name << " id: " << i << ", is eating 🍴 now" << endl;
+            }
+
+            randomSleep();
+
+            leftFork(id).unlock();
+            rightFork(id).unlock();
+
+            if (DEBUG) printf("Philosopher id: %i, released  forks\n", id);
+
+            {
+                unique_lock<mutex> tempLock(tableLock);
+                tableCount++;
+            }
+
+
+            if (DEBUG) printf("Philosopher id: %i, left the table\n", id);
+
+            {
+                const lock_guard<mutex> lock(printLock);
+                cout << setw(20) << name << " id: " << id << ", is full 🙂" << endl;
+            }
+
+
+        }
+    }
+};
+
+
+int main(int argc, char *argv[]) {
+
+    DEBUG = (argc == 2 && strcmp(argv[1], "-d") == 0);
+    printf("Dining-philosophers problem. Example with waiter\n");
+    if (DEBUG) printf("In debugging mode...\n");
+
+    tableLock.lock();
+    tableCount = NO_PHILOSOPHERS - 1;
+    tableLock.unlock();
+    {
+        const lock_guard<mutex> lock(printLock);
+        cout << "Create philosophers..." << endl;
+    }
+    Philosopher philosophers[NO_PHILOSOPHERS] = {{0, "Tales 👩"},
+                                                 {1, "Pitagoras 🧓"},
+                                                 {2, "Heraklit 🤶"},
+                                                 {3, "Platon 👳"},
+                                                 {4, "Arystoteles 👵"}};
+
+    {
+        const lock_guard<mutex> lock(printLock);
+        cout << "Create threads... " << endl;;
+    }
+    thread threads[NO_PHILOSOPHERS];
+    for (int i = 0; i < NO_PHILOSOPHERS; i++) {
+        threads[i] = thread(philosophers[i]);
+    }
+
+
+    {
+        const lock_guard<mutex> lock(printLock);
+        cout << "Wait for threads... " << endl;
+    }
+
+    for (auto &thread : threads) {
+        thread.join();
+    }
+
+    printf("Thank you for attention.\n");
+    return 0;
+}
